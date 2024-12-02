db0d47593374d61f902b02e82d27ec06d202fc4a
Seth Axen <seth@sethaxen.com>
2024-12-02 16:04:19 +0100

    Add solution to day 2 part 1

---
+++ src/bin/day02.rs
@@ -0,0 +1,52 @@
+use aoc::load_input;
+use itertools::Itertools;
+
+fn is_report_safe(report: &str) -> bool {
+    let levels = report.split_whitespace().map(|n| n.parse::<u32>().unwrap());
+    let unique_level_signs: Vec<i32> = levels
+        .tuple_windows()
+        .map(|(first, last)| {
+            let diff = (first as i32) - (last as i32);
+            return if diff.abs() > 3 || diff == 0 {
+                0
+            } else {
+                diff.signum()
+            };
+        })
+        .unique()
+        .collect();
+    return unique_level_signs.len() == 1 && unique_level_signs[0] != 0;
+}
+
+fn solve_part1(input: &str) -> usize {
+    let num_safe = input
+        .trim_end()
+        .lines()
+        .map(is_report_safe)
+        .filter(|b| *b)
+        .count();
+    return num_safe;
+}
+
+fn main() {
+    let input = load_input("day02.txt");
+    println!("Solution to part 1: {}", solve_part1(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
+    ";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(solve_part1(EXAMPLE_INPUT), 2);
+    }
+}
