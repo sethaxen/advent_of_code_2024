12797c9053c9ad454b80d366a908b3eeb5e02605
Seth Axen <seth@sethaxen.com>
2024-12-03 20:04:58 +0100

    Add solution to day 3 part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,7 +1,7 @@
 use aoc::load_input;
 use regex::Regex;
 
-fn solve_part1(input: &str) -> usize {
+fn compute_muls(input: &str) -> usize {
     let pattern = Regex::new(r"mul\((?<l>\d{1,3}),(?<r>\d{1,3})\)").unwrap();
     return pattern
         .captures_iter(input)
@@ -13,20 +13,47 @@ fn solve_part1(input: &str) -> usize {
         .sum();
 }
 
+fn solve_part1(input: &str) -> usize {
+    return compute_muls(input);
+}
+
+fn solve_part2(input: &str) -> usize {
+    return input
+        .split("don't()")
+        .enumerate()
+        .map(|(i, b)| {
+            if i == 0 {
+                return compute_muls(b);
+            } else {
+                let (_disabled, enabled) = b.split_once("do()").or(Some(("", ""))).unwrap();
+                return compute_muls(enabled);
+            }
+        })
+        .sum();
+}
+
 fn main() {
     let input = load_input("day03.txt");
     println!("Solution to part 1: {}", solve_part1(&input));
+    println!("Solution to part 1: {}", solve_part2(&input));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE_INPUT: &str =
+    const EXAMPLE_INPUT1: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const EXAMPLE_INPUT2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn test_part1() {
-        assert_eq!(solve_part1(EXAMPLE_INPUT), 161);
+        assert_eq!(solve_part1(EXAMPLE_INPUT1), 161);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(solve_part2(EXAMPLE_INPUT2), 48);
     }
 }
